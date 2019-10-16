a6f7dbb423f95c01a0616a79ffbdb45dc7601131
Victor Mourão <victor_murati@hotmail.com>
2019-10-15 23:25:46 -0300

    product duplicated and immer config

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   /*
     Switch para fazer alguma ação dependendo
@@ -6,13 +8,23 @@ export default function cart(state = [], action) {
   */
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      /*
+        O immer permite manipular o array como se fosse imutável
+        draft recebe o estado e pode fazer 'rascunhos' para alterar o estado
+      */
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          product => product.id === action.product.id
+        );
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
